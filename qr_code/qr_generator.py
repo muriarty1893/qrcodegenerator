2fda94ea67f55f004ce1dd3b5b7ace521d0d081a
muriarty1983 <murateker9166@gmail.com>
2024-10-08 12:22:53 +0300

    fixed unnecessary data that cause an error

--- qr_code/qr_generator.py
+++ qr_code/qr_generator.py
@@ -1,8 +1,20 @@
 import qrcode
 
 def create_qr_code(data, filename='qrcode.png'):
+    # Verinin uzunluğuna göre sürümü dinamik ayarlayın
+    if len(data) < 50:  # Örnek olarak, 50 karakterden küçükse
+        version = 1
+    elif len(data) < 150:
+        version = 2
+    elif len(data) < 300:
+        version = 3
+    elif len(data) < 600:
+        version = 4
+    else:
+        version = 40  # Maksimum sürüm
+
     qr = qrcode.QRCode(
-        version=1,
+        version=version,
         error_correction=qrcode.constants.ERROR_CORRECT_L,
         box_size=10,
         border=4,
